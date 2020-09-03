c3ea0e2a543588b1e14d5f6492503f6e4a52d2b4
Chad Way <waychad1@outlook.com>
2020-09-03 15:43:32 -0500

    2. Vehicle - complete

---
+++ ClassesLibrary/Studentinfo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesLibrary
+{
+    public class Student
+
+    #region Field
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        #endregion Field
+        #region Properties
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public float GPA
+        {
+            get { return _gpa; }
+            set
+            {
+                if (value < 0f)
+                { _gpa = 100; }
+                if (value > 4.0f)
+                { _gpa = 66; }
+                else
+                { _gpa = value; }
+            }
+        }
+
+        #endregion
+        #region Constructors
+
+        //custom constructor
+
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ID = id;
+            GPA = gpa;
+        }
+
+        //default constructor
+
+        public Student() { }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return string.Format("\nStudent Information:\n\nName: {0} {1}\nID: {2}\nGPA: {3}", FirstName, LastName, ID, GPA);
+        }
+
+        #endregion 
+
+    }
+}
---
+++ ClassesLibrary/Vehicle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesLibrary
+{
+    public class Vehicle
+
+    {
+        private string _make;
+        private string _model;
+        private int _year;
+        private float _weight;
+
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+        public float Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public Vehicle(string make, string model, int year, float weight)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Weight = weight;
+        }
+
+        public Vehicle() { }
+
+        public override string ToString()
+        {
+            return string.Format($"Car Information:\nMake:{Make}\nModel:{Model}\nYear:{Year}\nWeight:{Weight} lbs");
+        }
+    
+
+    }
+}
